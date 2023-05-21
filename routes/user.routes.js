2aee26fe4201f320fa1f784eda920047f5d08e6e
PMorgane <passarellimorgane43@gmail.com>
2023-05-21 18:31:02 +0200

    update

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,15 @@
 const router = require("express").Router();
 const authController=require("../controllers/authController")
-
+const userController = require("../controllers/user.controller.js");
 
 // auth
 router.post("/register", authController.signUp);
 
+
+//user display "block"
+router.get('/', userController.getAllUsers);
+router.get("/:id", userController.userInfo);
+router.put("/:id", userController.updateUser);
 
 
 module.exports=router;
